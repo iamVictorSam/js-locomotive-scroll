818d24a78e2f96a50557c7ccf4b626f3fe71b302
Sam Victor <iamvictorsam@gmail.com>
2023-06-29 19:23:57 +0100

    starter

--- index.js
+++
@@ -1,9 +0,0 @@
-import LocomotiveScroll from 'locomotive-scroll';
-
-const element = document.querySelector('[data-scroll-container]');
-
-const scroll = new LocomotiveScroll({
-    el: element,
-    smooth: true,
-    multiplier: 4,
-});
